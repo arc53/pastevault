2e8ec2708032c3370e2c675397798cd943955b03
Alex <a@tushynski.me>
2025-08-27 20:27:06 +0100

    refactor: simplify ESLint configuration to extend next/core-web-vitals

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,3 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-  },
-  env: {
-    node: true,
-    es6: true,
-  },
-}
+  extends: 'next/core-web-vitals',
+};
